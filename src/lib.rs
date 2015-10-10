4d8ac1a62210f2b5cdad26cde30143b28900e129
Sven Nilsen <bvssvni@gmail.com>
2015-10-10 21:34:14 +0200

    Handle references internally
    See https://github.com/PistonDevelopers/editor/issues/11

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,12 @@
 //! Editor interface.
 
 use std::any::Any;
-use std::sync::Arc;
 
 /// A generic interface for editors, implemented on controllers.
 ///
 /// Provides all information necessary to execute actions,
 /// select objects, navigate and update.
 /// This makes it possible to write reusable generic actions.
-///
-/// History is handled externally, using `Box<Any>` for changes.
-/// The editor should not store any history internally,
-/// unless it is garbage collected from within the `Any` data sent outside.
-///
-/// All changes must be reversible from the information that is sent outside.
-/// You can use `Arc` to check for uniqueness for resources with a handle.
-/// If the internal `Arc` is unique, it means the information can safely be
-/// removed. This must happen internally.
-///
-/// A history buffer must keep handles alive since they can not be recreated.
-/// If this is a problem, then the editor must map to a recreatable resource.
-///
-/// The controller should keep same selection state across multiple views.
-///
-/// References should not be handled internally.
-/// This is done through algorithms using the reference information.
-/// For example, before deleting an object, checks that all affected
-/// references are cascading, such there are no loose references after deletion.
-/// Cascading references deletes the objects that the reference points from.
 ///
 /// Methods that returns `Result` can trigger a rollback in actions.
 /// This is to prevent logical errors from affecting data.
@@ -61,27 +40,18 @@ pub trait Editor {
     fn insert(&mut self, ty: Type, args: &Any) -> Result<Object, ()>;
     /// Returns an object which references must be updated when
     /// using swap-remove by replacing object with last one in same table.
-    fn delete(&mut self, ty: Type, obj: Object) -> Result<Option<Object>, ()>;
+    fn delete(&mut self, ty: Type, obj: Object) -> Result<(), ()>;
     /// Updates an object with new values.
     fn update(&mut self, ty: Type, obj: Object, args: &Any) -> Result<(), ()>;
     /// Replaces an object with another.
-    /// Keeps references pointing to the old object, but deletes
-    /// references pointing from the old object.
-    fn replace(&mut self, ty: Type, from: Object, to: Object)
-    -> Result<Option<Object>, ()>;
-    /// Get the field of an object.
+    fn replace(&mut self, ty: Type, from: Object, to: Object) -> Result<(), ()>;
+    /// Get the value of an object.
     fn get<'a>(&'a self, ty: Type, obj: Object) -> Result<&'a Any, ()>;
-    /// Get references pointing to an object.
-    fn references_to(&self, ty: Type, obj: Object) -> Vec<Reference>;
-    /// Get references pointing from an object to other objects.
-    fn references_from(&self, ty: Type, obj: Object) -> Vec<Reference>;
-    /// Delete a reference without deleting the object containing it.
-    fn delete_reference(&mut self, reference: Reference) -> Result<(), ()>;
     /// Get the visible objects of a type.
     fn visible(&self, ty: Type) -> Vec<Object>;
     /// Gets the selected object of a type.
     /// If the editor supports multiple selection,
-    /// the selected object is usually the last in the selected list.
+    /// the selected object is usually the among the multiple-selected ones.
     fn selected(&self, ty: Type) -> Option<Object>;
     /// Gets the multiple selected objects of a type.
     /// The order of the selected objects matter.
@@ -90,18 +60,6 @@ pub trait Editor {
     fn all(&self, ty: Type) -> Vec<Object>;
     /// Navigate to an object such that it becomes visible.
     fn navigate_to(&mut self, ty: Type, obj: Object) -> Result<(), ()>;
-    /// Gets the types in the editor.
-    fn types(&self) -> Vec<Type>;
-    /// Get the fields of an object.
-    /// This requires an object because it can be dynamically typed.
-    /// Fields of statically types are known at compile time.
-    fn fields_of(&self, ty: Type, obj: Object) -> Vec<Field>;
-    /// Updates a field. This is used by property widgets.
-    fn update_field(&mut self, ty: Type, obj: Object, field: Field, val: &Any)
-    -> Result<(), ()>;
-    /// Refreshes the views.
-    /// This is called at the end of each action to update cached data.
-    fn refresh_views(&mut self);
 }
 
 /// The type of an object.
@@ -112,41 +70,6 @@ pub struct Type(pub &'static str);
 /// The object id.
 #[derive(Clone, Copy, Debug)]
 pub struct Object(pub usize);
-
-/// Stores information about a reference.
-#[derive(Clone, Debug)]
-pub struct Reference {
-    /// The type of the from object.
-    pub from_ty: Type,
-    /// The id of the from object.
-    pub from_obj: Object,
-    /// The type of the to object.
-    pub to_type: Type,
-    /// The id of the to object.
-    pub to_obj: Object,
-    /// Whether to delete objects using this reference.
-    /// When `false`, deletion will be cancelled with an error,
-    /// unless the reference is optional.
-    pub cascade: bool,
-    /// Whether to delete a reference without deleting the object itself.
-    /// If `cascade` is `true`, then the object will be deleted anyway.
-    pub optional: bool,
-    /// The field that points to an object.
-    pub field: Field,
-}
-
-/// Field information.
-#[derive(Clone, Debug)]
-pub struct Field {
-    /// The name of field.
-    pub name: Arc<String>,
-    /// The type of the field.
-    pub ty: Type,
-    /// The index within array, 0 for normal fields.
-    pub index: usize,
-    /// 0 for a normal named field, length for array.
-    pub array: usize,
-}
 
 /// A helper function for `Editor::delete` implementation.
 pub fn delete<T>(items: &mut Vec<T>, obj: Object) -> Result<Option<Object>, ()> {
